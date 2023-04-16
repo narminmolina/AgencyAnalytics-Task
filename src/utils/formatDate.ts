d31c9be081ccc32f0825435573f0bcdcf8c4a067
Narmin Molina <narminmolina@gmail.com>
2023-04-16 11:50:08 -0400

    testing set up and unit testing for utility functions

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,9 @@
 export const formatDate = (dateString: string) => {
-  const formatter = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+  const formatter = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'UTC',
+  });
   return formatter.format(new Date(dateString));
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react-swc';
-
 import tsconfigPaths from 'vite-tsconfig-paths';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), eslintPlugin()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'src/tests/_setup.ts',
+  },
 });
